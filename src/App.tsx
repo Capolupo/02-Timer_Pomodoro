2fdd43f18065df7c1ddc1182f7ba08b7cc969ddd
Andre Capolupo <andre.capolupo@gmail.com>
2024-11-18 22:48:27 -0300

    aplicando o defaulttheme e o global style ao app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
+import { ThemeProvider } from 'styled-components';
+
+import { GlobalStyle } from './styles/global';
+
+import { defaultTheme } from './styles/themes/default';
 //no mais tem que fazerr aimportação com chaves assim import { App } from './App.tsx'
 
 //O export function me obriga a utilizar o nome da função no caso app
 export function App() {
-  return (  
-    <h1>Hello ANdre</h1>
+  return ( 
+    /*posso ter varios arquivos de themas e no caso seria so trocar o 
+    defaultTheme para outroTheme*/ 
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+    </ThemeProvider> 
   )
 }
 
