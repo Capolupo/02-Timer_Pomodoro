d0876b4247fcd7e119a3fd5207c694641e5c0bd9
Andre Capolupo <andre.capolupo@gmail.com>
2024-11-18 23:24:44 -0300

    ajustes eslint

--- src/@types/styles.d.ts
+++ src/@types/styles.d.ts
@@ -1,10 +1,10 @@
 import 'styled-components';
 
-import { defaultTheme } from '../styles/themes/default';
+import { defaultTheme } from "../styles/themes/default";
 
 type ThemeType = typeof defaultTheme;
 
-//cria uma tipagem para o npm
+//cria uma tipagem para o modulo styled-components do npm
 declare module 'styled-components' {
   export interface DefaultTheme extends ThemeType {}
 }
